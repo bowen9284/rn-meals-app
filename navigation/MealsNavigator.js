2fb8671f33be6db460cde68ed775d7b88f9247b0
Matt Bowen <bowen9284@gmail.com>
2020-07-16 21:33:28 -0400

    adding fonts and react navigation

---
+++ navigation/MealsNavigator.js
@@ -0,0 +1,15 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+import CategoriesScreen from '../screens/CategoriesScreen';
+import CategoryMealsScreen from '../screens/CategoryMealsScreen';
+import MealDetailsScreen from '../screens/MealDetailScreen';
+
+const MealsNavigator = createStackNavigator({
+  Categories: CategoriesScreen,
+  CategoryMeal: {
+    screen: CategoryMealsScreen,
+  },
+  MealDetail: MealDetailsScreen,
+});
+
+export default createAppContainer(MealsNavigator);
